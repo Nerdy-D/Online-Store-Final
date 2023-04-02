228879df8c979898876914930630672f4707d9b2
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-04-02 22:59:19 +0200

    everything working

--- BuyNowPage/majorCode.js
+++ BuyNowPage/majorCode.js
@@ -9,37 +9,155 @@ var app = new Vue({
                 price: 'R5.00',
             },
             {
+                name: 'Bread',
+                image: 'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Dough',
+                image: 'https://images.pexels.com/photos/9510/food-pizza-kitchen-recipe.jpg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R10.00',
+            },
+            {
+                name: 'Icing',
+                image: 'https://media.istockphoto.com/id/1306194391/photo/woman-decorates-freshly-baked-cupcakes-with-cream.jpg?b=1&s=612x612&w=0&k=20&c=bo-BQgqHvbsaHdVLydGWYmUg2df-NNBQlIfsdKXQbyY=',
+                description: 'Yummy',
+                price: 'R7.00',
+            },
+            {
+                name: 'Chocolate',
+                image: 'https://images.pexels.com/photos/4110101/pexels-photo-4110101.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R11.00',
+            },
+            {
+                name: 'Scones',
+                image: 'https://images.pexels.com/photos/7686487/pexels-photo-7686487.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Cream',
+                image: 'https://media.istockphoto.com/id/515777808/photo/yogurt.jpg?b=1&s=612x612&w=0&k=20&c=i1i529QwDw4KCY2pXJDBi45YvAdrh8Wit9b-TzgD56s=',
+                description: 'Yummy',
+                price: 'R12.00',
+            },
+            {
                 name: 'Milk',
                 image: 'https://images.pexels.com/photos/5836625/pexels-photo-5836625.jpeg?auto=compress&cs=tinysrgb&w=1600',
                 description: 'Yummy',
                 price: 'R5.00',
             },
+            {
+                name: 'Bread',
+                image: 'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Dough',
+                image: 'https://images.pexels.com/photos/9510/food-pizza-kitchen-recipe.jpg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R10.00',
+            },
+            {
+                name: 'Icing',
+                image: 'https://media.istockphoto.com/id/1306194391/photo/woman-decorates-freshly-baked-cupcakes-with-cream.jpg?b=1&s=612x612&w=0&k=20&c=bo-BQgqHvbsaHdVLydGWYmUg2df-NNBQlIfsdKXQbyY=',
+                description: 'Yummy',
+                price: 'R7.00',
+            },
+            {
+                name: 'Chocolate',
+                image: 'https://images.pexels.com/photos/4110101/pexels-photo-4110101.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R11.00',
+            },
+            {
+                name: 'Scones',
+                image: 'https://images.pexels.com/photos/7686487/pexels-photo-7686487.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Cream',
+                image: 'https://media.istockphoto.com/id/515777808/photo/yogurt.jpg?b=1&s=612x612&w=0&k=20&c=i1i529QwDw4KCY2pXJDBi45YvAdrh8Wit9b-TzgD56s=',
+                description: 'Yummy',
+                price: 'R12.00',
+            },
+            {
+                name: 'Milk',
+                image: 'https://images.pexels.com/photos/5836625/pexels-photo-5836625.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R5.00',
+            },
+            {
+                name: 'Bread',
+                image: 'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Dough',
+                image: 'https://images.pexels.com/photos/9510/food-pizza-kitchen-recipe.jpg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R10.00',
+            },
+            {
+                name: 'Icing',
+                image: 'https://media.istockphoto.com/id/1306194391/photo/woman-decorates-freshly-baked-cupcakes-with-cream.jpg?b=1&s=612x612&w=0&k=20&c=bo-BQgqHvbsaHdVLydGWYmUg2df-NNBQlIfsdKXQbyY=',
+                description: 'Yummy',
+                price: 'R7.00',
+            },
+            {
+                name: 'Chocolate',
+                image: 'https://images.pexels.com/photos/4110101/pexels-photo-4110101.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R11.00',
+            },
+            {
+                name: 'Scones',
+                image: 'https://images.pexels.com/photos/7686487/pexels-photo-7686487.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Cream',
+                image: 'https://media.istockphoto.com/id/515777808/photo/yogurt.jpg?b=1&s=612x612&w=0&k=20&c=i1i529QwDw4KCY2pXJDBi45YvAdrh8Wit9b-TzgD56s=',
+                description: 'Yummy',
+                price: 'R12.00',
+            },
         ],
         cart: [],
     },
+    computed: {
+    },
     created: function () {
 
-        const jsoncart = localStorage.getItem('addToCart');
+        const jsoncart = localStorage.getItem('cart');
         console.log(jsoncart);
-        if (jsoncart) this.watchlist = JSON.parse(jsoncart);
+        if (jsoncart) this.cart = JSON.parse(jsoncart);
     },
 
     methods: {
+
+
         onAddToCart: function (product) {
 
-            this.addToCart.push(product);
-            const jsoncart = JSON.stringify(this.addToCart);
-            localStorage.setItem('cart', jsoncart);
+            this.cart.push(product);
+            const jsoncart = JSON.stringify(this.cart);
+            localStorage.setItem("cart", jsoncart);
         },
         onRemoveItem: function (product) {
 
-            const index = this.addToCart.indexOf(product);
+            const index = this.cart.indexOf(product);
 
             if (index > -1) {
-                this.addToCart.splice(index, 1);
-                const jsoncart = JSON.stringify(this.addToCart);
+                this.cart.splice(index, 1);
+                const jsoncart = JSON.stringify(this.cart);
 
-                localStorage.setItem('addToCart', jsoncart);
+                localStorage.setItem("cart", jsoncart);
             }
         },
     },
--- Components/product.js
+++ Components/product.js
@@ -2,13 +2,13 @@ Vue.component('product', {
     props: ['name', 'image', 'description', 'price', 'show-add', 'show-remove'],
     template: `
     <div class="product">
-    <img v-bind:src="image" width="150px">
+    <img v-bind:src="image" width="190px" height="250px">
     <h3 class="name">
     {{name}}
     </h3>
     <p class="description">{{description}}</p>
     <p class="price">{{price}}</p>
-    <button v-if="showAdd" v-on:click="$emit('addToCart')">Add</button>
-    <button v-if="showRemove" v-on:click="$emit('removeFromCart')">Remove</button>
+    <button v-if="showAdd" v-on:click="$emit('add-to-cart')">Add</button>
+    <button v-if="showRemove" v-on:click="$emit('remove-from-cart')">Remove</button>
     </div>`,
 });
