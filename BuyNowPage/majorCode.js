789f3a8e5c004ae798e2f70d0afe56e6067054d0
Nerdy-D <111739562+Nerdy-D@users.noreply.github.com>
2023-04-02 23:51:05 +0200

    Finished - Online Store

--- BuyNowPage/majorCode.js
+++ BuyNowPage/majorCode.js
@@ -129,6 +129,59 @@ var app = new Vue({
                 price: 'R12.00',
             },
         ],
+        productNew: [
+            {
+                name: 'Milk',
+                image: 'https://images.pexels.com/photos/5836625/pexels-photo-5836625.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R5.00',
+            },
+            {
+                name: 'Bread',
+                image: 'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Dough',
+                image: 'https://images.pexels.com/photos/9510/food-pizza-kitchen-recipe.jpg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R10.00',
+            },
+            {
+                name: 'Icing',
+                image: 'https://media.istockphoto.com/id/1306194391/photo/woman-decorates-freshly-baked-cupcakes-with-cream.jpg?b=1&s=612x612&w=0&k=20&c=bo-BQgqHvbsaHdVLydGWYmUg2df-NNBQlIfsdKXQbyY=',
+                description: 'Yummy',
+                price: 'R7.00',
+            },
+            {
+                name: 'Milk',
+                image: 'https://images.pexels.com/photos/5836625/pexels-photo-5836625.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R5.00',
+            },
+        ],
+        productFeature: [
+            {
+                name: 'Chocolate',
+                image: 'https://images.pexels.com/photos/4110101/pexels-photo-4110101.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R11.00',
+            },
+            {
+                name: 'Scones',
+                image: 'https://images.pexels.com/photos/7686487/pexels-photo-7686487.jpeg?auto=compress&cs=tinysrgb&w=1600',
+                description: 'Yummy',
+                price: 'R8.00',
+            },
+            {
+                name: 'Cream',
+                image: 'https://media.istockphoto.com/id/515777808/photo/yogurt.jpg?b=1&s=612x612&w=0&k=20&c=i1i529QwDw4KCY2pXJDBi45YvAdrh8Wit9b-TzgD56s=',
+                description: 'Yummy',
+                price: 'R12.00',
+            },
+
+        ],
         cart: [],
     },
     computed: {
